0a50906df1e5e3b187414a429c50b7446a58e208
MelKV <melanie.verdet@free.fr>
2023-10-26 11:58:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omeg 3 & Bleberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
